0b95c63a4d2d5e01cce997a0c8624e59c793bf1b
Danhlq <quangdanh5791@gmail.com>
2025-11-10 21:09:55 +0700

    .

--- .idea/scrip.js
+++ .idea/scrip.js
@@ -1,2 +1,10 @@
-var intro="hello word";
-document.getElementById("intro").innerHTML=intro;
+function square(number) {
+    let result = number * number;
+    return result;
+
+}
+var squareof2 = square(2);
+document.write("bt của 2 là: " + squareof2);
+
+var squareof5 = square(5);
+document.write("bt của 5 là: " + squareof5);
